8e6104e18d4944aac935d20b2a974a77047e1edb
Feyannibelli <fyann06@gmail.com>
2025-06-03 13:25:30 -0300

    a

---
+++ engine/src/main/java/edu/austral/dissis/chess/engine/model/game/Game.java
@@ -0,0 +1,4 @@
+package edu.austral.dissis.chess.engine.model.game;
+
+public class Game {
+}
---
+++ engine/src/main/java/edu/austral/dissis/chess/engine/model/piece/movement/special/DoubleStepPawnStrategy.java
@@ -0,0 +1,4 @@
+package edu.austral.dissis.chess.engine.model.piece.movement.special;
+
+public class DoubleStepPawnStrategy {
+}
